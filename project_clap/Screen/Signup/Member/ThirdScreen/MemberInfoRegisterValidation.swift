d093ae808a7b280f39044389c7c79379a5117d88
y.omura <y.omura@caraquri.com>
2019-03-04 19:04:39 +0900

    メンバー登録作成ViewModel

--- project_clap/Screen/Signup/Member/ThirdScreen/MemberInfoRegisterValidation.swift
+++ project_clap/Screen/Signup/Member/ThirdScreen/MemberInfoRegisterValidation.swift
@@ -1,9 +1,50 @@
-//
-//  MemberInfoRegisterValidation.swift
-//  project_clap
-//
-//  Created by オムラユウキ on 2019/03/04.
-//  Copyright © 2019 Swifter. All rights reserved.
-//
-
 import Foundation
+
+enum MemberInfoRegisterValidationResult {
+    case ok
+    case empty
+    case notMatchEmail
+    case notMatchPass
+}
+
+extension MemberInfoRegisterValidationResult {
+    var isValid: Bool {
+        switch self {
+        case .ok:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+struct MemberInfoRegisterValidation {
+    static func validatePass(pass: String, rePass: String) -> MemberInfoRegisterValidationResult {
+        guard pass == rePass else {
+            return .notMatchPass
+        }
+        return .ok
+    }
+    
+    static func validateEmail(email: String) -> MemberInfoRegisterValidationResult {
+        let emailCheck = NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
+        guard emailCheck.evaluate(with: email) else {
+            return .notMatchEmail
+        }
+        return .ok
+    }
+    
+    static func validateEmpty(name: String, mail: String, pass: String, rePass: String) -> MemberInfoRegisterValidationResult {
+        guard name.count >= 3, mail.count >= 3, pass.count >= 3, rePass.count >= 3 else {
+            return .empty
+        }
+        return .ok
+    }
+    
+    static func validatePicker(position: String) -> MemberInfoRegisterValidationResult {
+        guard position != R.string.locarizable.empty() else {
+            return .empty
+        }
+        return .ok
+    }
+}
--- project_clap/Screen/Signup/Member/ThirdScreen/MemberInfoRegisterViewModel.swift
+++ project_clap/Screen/Signup/Member/ThirdScreen/MemberInfoRegisterViewModel.swift
@@ -1,9 +1,69 @@
-//
-//  MemberInfoRegisterViewModel.swift
-//  project_clap
-//
-//  Created by オムラユウキ on 2019/03/04.
-//  Copyright © 2019 Swifter. All rights reserved.
-//
-
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol MemberInfoRegisterViewModelInput {
+    var nameText: Observable<String> { get }
+    var mailText: Observable<String> { get }
+    var passText: Observable<String> { get }
+    var rePassText: Observable<String> { get }
+    var memberPosition: Observable<String> { get }
+    var registBtnTap: Observable<()> { get }
+}
+
+protocol MemberInfoRegisterViewModelOutput {
+    var isRegistBtnEnable: Observable<Bool> { get }
+}
+
+protocol MemberInfoRegisterViewModelType {
+    var inputs: MemberInfoRegisterViewModelInput { get }
+    var outputs: MemberInfoRegisterViewModelOutput { get }
+}
+
+struct MemberInfoRegisterViewModel: MemberInfoRegisterViewModelType, MemberInfoRegisterViewModelInput, MemberInfoRegisterViewModelOutput {
+    var inputs: MemberInfoRegisterViewModelInput { return self }
+    var outputs: MemberInfoRegisterViewModelOutput { return self }
+    var nameText: Observable<String>
+    var mailText: Observable<String>
+    var passText: Observable<String>
+    var rePassText: Observable<String>
+    var registBtnTap: Observable<()>
+    var memberPosition: Observable<String>
+    var isRegistBtnEnable: Observable<Bool>
+    
+    init(nameField: Observable<String>, mailField: Observable<String>, passField: Observable<String>, rePassField: Observable<String>, positionField: Observable<String>, registBtn: Observable<()>) {
+        nameText = nameField
+        mailText = mailField
+        passText = passField
+        rePassText = rePassField
+        memberPosition = positionField
+        registBtnTap = registBtn
+        
+        let isEmptyField = Observable.combineLatest(nameText, mailText, passText, rePassText) { name, mail, pass, rePass -> MemberInfoRegisterValidationResult in
+            return MemberInfoRegisterValidation.validateEmpty(name: name, mail: mail, pass: pass, rePass: rePass)
+        }
+        .share(replay: 1)
+        
+        let passFieldWhetherMatch = Observable.combineLatest(passText, rePassText) { pass, repass -> MemberInfoRegisterValidationResult in
+            return MemberInfoRegisterValidation.validatePass(pass: pass, rePass: repass)
+        }
+        .share(replay: 1)
+        
+        let emailFieldWhetherMatch = Observable.combineLatest([mailText], { mail -> MemberInfoRegisterValidationResult in
+            return MemberInfoRegisterValidation.validateEmail(email: mail[0])
+        })
+        .share(replay: 1)
+        
+        let isEmptyPicker = Observable.combineLatest([memberPosition]) { position -> MemberInfoRegisterValidationResult in
+            return MemberInfoRegisterValidation.validatePicker(position: position[0])
+        }
+        
+        isRegistBtnEnable = Observable.combineLatest(isEmptyField, passFieldWhetherMatch, emailFieldWhetherMatch, isEmptyPicker) { (empty, pass, mail, picker) in
+            empty.isValid &&
+                pass.isValid &&
+                mail.isValid &&
+                picker.isValid
+            }
+            .share(replay: 1)
+    }
+}
